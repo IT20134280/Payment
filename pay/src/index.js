e78a4f54fdba7d3ab8f2fdcc57d17ace6bdbb33d
Ishalini <87859854+IT20134280@users.noreply.github.com>
2022-05-19 15:08:29 +0530

    update

--- pay/src/index.js
+++ pay/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PaymentDetails from "./moviePayment/PaymentDetails";
+//import PaymentDetails from "./moviePayment/PaymentDetails";
 //import PaymentDisplay from "./adminPayment/PaymentDisplay";
 //import App from './App';
-import App from "./moviePayment/App";
+//mport App from "./moviePayment/App";
+import CreditCardForm from './moviePayment/components/CreditCardForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CreditCardForm />
   </React.StrictMode>
 );
 
