ac45684744636b47d6c1cd4108664793bd71c108
Ishalini <87859854+IT20134280@users.noreply.github.com>
2022-05-19 18:59:08 +0530

    update

--- pay/src/moviePayment/components/Header.js
+++ pay/src/moviePayment/components/Header.js
@@ -8,7 +8,17 @@ export default function Header() {
             <header>
                 <div>
                     <h2>Payment</h2>
-                    <img class="img"  src={QRcode} />
+                    <ul>
+                        <li>
+                        <h2>Beast</h2>
+            <p>savoy 3D Doolby ATMOS - Wellawatte</p>
+            <h3>Tickets 3</h3>
+                        </li>
+                        <li>
+                        <img class="img"  src={QRcode} />
+                        </li>
+                    </ul>
+                    
                 </div>
             </header>
         </>
